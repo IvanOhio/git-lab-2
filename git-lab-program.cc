67209ffc33992ba4becab18d06be5a1c862c6707
Ivan Dotson <id339223@ohio.edu>
2024-09-06 17:50:15 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Ivan Dotson>
+ *        Date: <09/06/24>
+ * Description: Lab 2 C++ code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
